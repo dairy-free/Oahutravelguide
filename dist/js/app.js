48e6a7d73a240cdfca31682fe4a4e61ab3903601
dairy-free <67081832+dairy-free@users.noreply.github.com>
2021-06-14 18:44:52 -0700

    oahu

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,35 +3,28 @@
 const navBtn = document.getElementsByClassName('toggle-nav')[0]
 const closeBtn = document.getElementsByClassName('close-btn')[0]
 const overlayScroll = document.getElementsByTagName('BODY')[0];
+const navbarContainer = document.getElementsByClassName('navbar-container')[0]
+
+
 
 
 closeBtn.addEventListener('click', toggleNav);
 navBtn.addEventListener('click', toggleNav);
 
 function toggleNav() {
-  const toggleOverlay = document.getElementsByClassName('nav-overlay')[0]
-  toggleOverlay.classList.toggle('show-nav')
+  navbarContainer.classList.toggle('show-nav')
   overlayScroll.classList.toggle('hide-scroll')
 }
 
 
-// Scroll effect on overlay
-const checkpoint = 300;
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll <= checkpoint) {
-    opacity = 1 - currentScroll / checkpoint;
-  } 
-  document.querySelector(".h1-cover-overlay").style.opacity = opacity;
-});
-
-// End Scroll effect on overlay
-
 // Images animations
 // Detect request animation frame
+
 let scroll = window.requestAnimationFrame ||
-             // IE Fallback
-             function(callback){ window.setTimeout(callback, 1000/60)};
+
+// IE Fallback
+
+function(callback){ window.setTimeout(callback, 1000/60)};
 let elementsToShow = document.querySelectorAll('.show-on-scroll'); 
 
 function loop() {
@@ -50,7 +43,6 @@ function loop() {
 // Call the loop for the first time
 loop();
 
-// Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
   // special bonus for those using jQuery
   if (typeof jQuery === "function" && el instanceof jQuery) {
@@ -67,4 +59,23 @@ function isElementInViewport(el) {
     (rect.top >= 0 &&
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
+}
+
+// Read more BTN on Planning Page
+const dotPara = document.getElementById('dots');
+const moreText = document.getElementById('more')
+const showBtn = document.getElementById('readBtn')
+
+showBtn.addEventListener('click', showMore)
+
+function showMore() {
+  if (dotPara.style.display === 'none') {
+    dotPara.style.display = "inline";
+    showBtn.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dotPara.style.display = "none";
+    showBtn.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
 }
