5cd1fe79fa15a07c2e6d194ee02e4c4377d7e2f3
dairy-free <67081832+dairy-free@users.noreply.github.com>
2021-06-18 03:01:26 -0700

    oahu

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,81 +1,76 @@
-// Nav Overlay 
+// Navbar
 
-const navBtn = document.getElementsByClassName('toggle-nav')[0]
-const closeBtn = document.getElementsByClassName('close-btn')[0]
+const navBtn = document.getElementsByClassName('toggle-nav')[0];
+const closeBtn = document.getElementsByClassName('close-btn')[0];
 const overlayScroll = document.getElementsByTagName('BODY')[0];
-const navbarContainer = document.getElementsByClassName('navbar-container')[0]
-
-
-
+const navbarContainer = document.getElementsByClassName('navbar-container')[0];
 
 closeBtn.addEventListener('click', toggleNav);
 navBtn.addEventListener('click', toggleNav);
 
 function toggleNav() {
-  navbarContainer.classList.toggle('show-nav')
-  overlayScroll.classList.toggle('hide-scroll')
+	navbarContainer.classList.toggle('show-nav');
+	overlayScroll.classList.toggle('hide-scroll');
 }
 
+// End Navbar
 
 // Images animations
 // Detect request animation frame
 
-let scroll = window.requestAnimationFrame ||
+let scroll =
+	window.requestAnimationFrame ||
+	// IE Fallback
 
-// IE Fallback
-
-function(callback){ window.setTimeout(callback, 1000/60)};
-let elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+	function(callback) {
+		window.setTimeout(callback, 1000 / 60);
+	};
+let elementsToShow = document.querySelectorAll('.show-on-scroll');
 
 function loop() {
+	Array.prototype.forEach.call(elementsToShow, function(element) {
+		if (isElementInViewport(element)) {
+			element.classList.add('is-visible');
+		} else {
+			element.classList.remove('is-visible');
+		}
+	});
 
-    Array.prototype.forEach.call(elementsToShow, function(element){
-      if (isElementInViewport(element)) {
-        element.classList.add('is-visible');
-      } else {
-        element.classList.remove('is-visible');
-      }
-    });
-
-    scroll(loop);
+	scroll(loop);
 }
 
 // Call the loop for the first time
 loop();
 
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
+	// special bonus for those using jQuery
+	if (typeof jQuery === 'function' && el instanceof jQuery) {
+		el = el[0];
+	}
+	var rect = el.getBoundingClientRect();
+	return (
+		(rect.top <= 0 && rect.bottom >= 0) ||
+		(rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+		(rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+	);
 }
 
 // Read more BTN on Planning Page
 const dotPara = document.getElementById('dots');
-const moreText = document.getElementById('more')
-const showBtn = document.getElementById('readBtn')
+const moreText = document.getElementById('more');
+const showBtn = document.getElementById('readBtn');
 
-showBtn.addEventListener('click', showMore)
+showBtn.addEventListener('click', showMore);
 
 function showMore() {
-  if (dotPara.style.display === 'none') {
-    dotPara.style.display = "inline";
-    showBtn.innerHTML = "Read More";
-    moreText.style.display = "none";
-  } else {
-    dotPara.style.display = "none";
-    showBtn.innerHTML = "Read Less";
-    moreText.style.display = "inline";
-  }
+	if (dotPara.style.display === 'none') {
+		dotPara.style.display = 'inline';
+		showBtn.innerHTML = 'Read More';
+		moreText.style.display = 'none';
+	} else {
+		dotPara.style.display = 'none';
+		showBtn.innerHTML = 'Read Less';
+		moreText.style.display = 'inline';
+	}
 }
